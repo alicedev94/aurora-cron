0bdebe783d0c4f088a64545117978ecc71bf43b2
alicedev94 <alicedev94@gmail.com>
2024-12-10 19:13:02 -0400

    funtional

--- index.js
+++ index.js
@@ -2,50 +2,52 @@ import { CronJob } from "cron";
 import axios from "axios";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+// this is very hard 
 
-setInterval(()=> {
-  socket.emit('showLog');
-  console.log('sss');
-}, 1000)
+// const socket = io("http://localhost:3002");
 
-// const interval = 1;
+// setInterval(()=> {
+//   socket.emit('showLog');
+//   console.log('sss');
+// }, 1000)
 
-// const job = new CronJob(
-//   `*/${interval}  * * * *`,
-//   async () => {
-//     try {
-//       // start
-//       const response = await axios.get(
-//         "http://localhost:3001/api/v1/calls-services/set-incremental"
-//       );
+const interval = 1;
 
-//       if (response.data?.insert_this) {
-//         const callServicesAdded = response.data.insert_this.join(", ");
-//         await axios.post("http://localhost:3001/api/v1/create-log", {
-//           description: `Call services successfully created: ${callServicesAdded}`,
-//           status: "start", // remplace for dynamic phrase.
-//         });
+const job = new CronJob(
+  `*/${interval}  * * * *`,
+  async () => {
+    try {
+      // start
+      const response = await axios.get(
+        "http://localhost:3001/api/v1/calls-services/set-incremental"
+      );
 
-//         // update frontend-web
-//         socket.emit("showLog");
-//       } else {
-//         await axios.post("http://localhost:3001/api/v1/create-log", {
-//           description: `Failed to create call services`,
-//           status: "error", // remplace for dynamic phrase.
-//         });
+      if (response.data?.insert_this) {
+        const callServicesAdded = response.data.insert_this.join(", ");
+        await axios.post("http://localhost:3001/api/v1/create-log", {
+          description: `Call services successfully created: ${callServicesAdded}`,
+          status: "start", // remplace for dynamic phrase.
+        });
 
-//         // update frontend-web
-//         socket.emit("showLog");
-//       }
-//     } catch (error) {
-//       console.error("Error al hacer la solicitud:", error);
-//     }
-//   },
-//   null,
-//   true,
-//   "America/Caracas"
-// );
+        // update frontend-web
+        socket.emit("showLog");
+      } else {
+        await axios.post("http://localhost:3001/api/v1/create-log", {
+          description: `Failed to create call services`,
+          status: "error", // remplace for dynamic phrase.
+        });
 
-// // Start job in cron.
-// job.start();
+        // update frontend-web
+        socket.emit("showLog");
+      }
+    } catch (error) {
+      console.error("Error al hacer la solicitud:", error);
+    }
+  },
+  null,
+  true,
+  "America/Caracas"
+);
+
+// Start job in cron.
+job.start();
