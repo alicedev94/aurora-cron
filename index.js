5af263b6bf9bb81f7b5f37dfd7417fb227bd256b
alicedev94 <alicedev94@gmail.com>
2024-12-10 18:01:42 -0400

    aa

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-import { CronJob } from 'cron';
-import axios from 'axios';
+import { CronJob } from "cron";
+import axios from "axios";
 
-const interval = 1; 
+const interval = 1;
 
-const job = new CronJob(`*/${interval}  * * * *`, async () => {
-  try {
-    // start 
-    const response = await axios.get('http://localhost:3001/api/v1/calls-services/set-incremental');
-    console.log('start');
-    // socket.emit('showLog')
+const job = new CronJob(
+  `*/${interval}  * * * *`,
+  async () => {
+    try {
+      // start
+      const response = await axios.get(
+        "http://localhost:3001/api/v1/calls-services/set-incremental"
+      );
 
-    if (response.data?.insert_this) {
-      const callServicesAdded = response.data.insert_this.join(", ");
-      await axios.post('http://localhost:3001/api/v1/create-log', {
-        description: `Call services successfully created: ${callServicesAdded}`,
-        status: 'start' // remplace for dynamic phrase.
-      });
-    } else {
-      await axios.post('http://localhost:3001/api/v1/create-log', {
-        description: `Failed to create call services`,
-        status: 'error' // remplace for dynamic phrase.
-      });
+      if (response.data?.insert_this) {
+        const callServicesAdded = response.data.insert_this.join(", ");
+        await axios.post("http://localhost:3001/api/v1/create-log", {
+          description: `Call services successfully created: ${callServicesAdded}`,
+          status: "start", // remplace for dynamic phrase.
+        });
+
+        // update frontend-web
+        // socket.emit("showLog");
+      } else {
+        await axios.post("http://localhost:3001/api/v1/create-log", {
+          description: `Failed to create call services`,
+          status: "error", // remplace for dynamic phrase.
+        });
+
+        // update frontend-web
+        // socket.emit("showLog");
+      }
+    } catch (error) {
+      console.error("Error al hacer la solicitud:", error);
     }
-
-    console.log('finish');
-  } catch (error) {
-    console.error('Error al hacer la solicitud:', error);
-  }
-}, null, true, 'America/Caracas');
+  },
+  null,
+  true,
+  "America/Caracas"
+);
 
 // Iniciar el trabajo cron
 job.start();
-
