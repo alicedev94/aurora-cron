acb8d4bde615358de047a0e7f6607eee69163e9a
Ivan Alvarez <sistemas.ialvarez93@gmail.com>
2025-02-13 13:49:09 -0400

    refactor synchronization logic and improve error handling for service calls

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 /**
- * Importing the classes for the synchronization of business partners
+ * Importing the classes for the synchronization of business partners, products, service calls, and tabulators
  */
 import BusinessPartnerJob from "./services/jobs/BUSSINESS_PARTNER/main.job.js";
-
-/**
- * Importing the classes for the synchronization of products
- */
 import Products from "./services/jobs/PRODUCTS/main.job.js";
 import ProductsMobile from "./services/jobs/PRODUCTS/mobile.job.js";
 import TechniciansMobile from "./services/jobs/TECHNICIANS/main.job.js";
-
-// Call Services
 import ServiceCall from "./services/jobs/SERVICE_CALLS/main.job.js";
 import ServiceCallMobile from "./services/jobs/SERVICE_CALLS/mobile.job.js";
 import ServiceCallServer from "./services/jobs/SERVICE_CALLS/server.job.js";
-
-// Tabulator
 import Tabulator from "./services/jobs/TABULATOR/main.job.js";
 import TabulatorMobile from "./services/jobs/TABULATOR/mobile.job.js";
 
 /** *
  * API URL AND PORT
  */
-
 const API = process.env.API;
 const PORT = process.env.PORT;
 
@@ -30,95 +21,96 @@ console.log(`API: ${API}`);
 
 /**
  * Configuration for business partner synchronization
- * Execution interval: 1 hour (60 minutes)
- * Limit of elements to synchronize: 1000
  */
 const businessPartnerLimit = 1000;
 const businessPartnerInterval = 60; // Interval in minutes for 1 hour
 
-const syncBusinessPartner = new BusinessPartnerJob(businessPartnerInterval, businessPartnerLimit);
+const syncBusinessPartner = new BusinessPartnerJob(
+  businessPartnerInterval,
+  businessPartnerLimit
+);
 syncBusinessPartner.startJob();
 
 /**
  * Configuration for product synchronization
- * Execution interval: 3 minutes
- * Limit of elements to synchronize: 1000
  */
 const productsLimit = 1000;
 const productsInterval = 3; // Interval in minutes
 
-// const syncProducts = new Products(productsInterval, productsLimit);
-// syncProducts.startJob();
+const syncProducts = new Products(productsInterval, productsLimit);
+syncProducts.startJob();
 
-/**
- * Configuration for product synchronization
- * Execution interval: 5 minutes
- * Limit of elements to synchronize: 1000
- */
 const productsMobileLimit = 1000;
 const productsMobileInterval = 120; // Interval in minutes
 
-const syncProductsWithAppMobile = new ProductsMobile(productsMobileInterval, productsMobileLimit);
+const syncProductsWithAppMobile = new ProductsMobile(
+  productsMobileInterval,
+  productsMobileLimit
+);
 syncProductsWithAppMobile.startJob();
 
 /**
- * Configuration for service call syncronization
+ * Configuration for service call synchronization
  */
-const serviceCallLimit = 300;  // Does not work, ilustrative
-const serviceCallInterval = 10; // Interval in minutes
+async function startServiceCallJobs() {
+  const serviceCallIntervalMinutes = 10;
+  const serviceCallLimit = 300;
 
-const serviceCallMobileLimit = 350;  // Does not work, ilustrative
-const serviceCallMobileInterval = 6; // Interval in minutes
+  const serviceCallMobileIntervalMinutes = 6;
+  const serviceCallMobileLimit = 350;
 
-const serviceCallServerLimit = 350;  // Does not work, ilustrative
-const serviceCallServerInterval = 10; // Interval in minutes
+  const serviceCallServerIntervalMinutes = 10;
+  const serviceCallServerLimit = 350;
 
-const techniciansMobileLimit = 1000;
-const getNewServiceCalls = new ServiceCall(serviceCallInterval,serviceCallLimit)
+  const techniciansMobileLimit = 1000;
 
-async function startJobs() {
   try {
+    const getNewServiceCalls = new ServiceCall(
+      serviceCallIntervalMinutes,
+      serviceCallLimit
+    );
     getNewServiceCalls.startJob();
   } catch (error) {
-    console.error('Error in getNewServiceCalls:', error);
+    console.error("Error in getNewServiceCalls:", error);
   }
 
   try {
     const syncTechniciansMobile = new TechniciansMobile(techniciansMobileLimit);
     syncTechniciansMobile.startJob();
   } catch (error) {
-    console.error('Error in syncTechniciansMobile:', error);
+    console.error("Error in syncTechniciansMobile:", error);
   }
 
   try {
-    const sendNewServiceCalls = new ServiceCallMobile(serviceCallMobileLimit, serviceCallMobileInterval);
+    const sendNewServiceCalls = new ServiceCallMobile(
+      serviceCallMobileIntervalMinutes,
+      serviceCallMobileLimit
+    );
     sendNewServiceCalls.startJob();
   } catch (error) {
-    console.error('Error in sendNewServiceCalls:', error);
+    console.error("Error in sendNewServiceCalls:", error);
   }
 
   try {
-    const getUpdatedServiceCalls = new ServiceCallServer(serviceCallServerLimit, serviceCallServerInterval);
+    const getUpdatedServiceCalls = new ServiceCallServer(
+      serviceCallServerIntervalMinutes,
+      serviceCallServerLimit
+    );
     getUpdatedServiceCalls.startJob();
   } catch (error) {
-    console.error('Error in getUpdatedServiceCalls:', error);
+    console.error("Error in getUpdatedServiceCalls:", error);
   }
 }
-
-startJobs().catch(console.error);
+startServiceCallJobs().catch(console.error);
 
 /**
  * Configuration for tabulator
  */
-const TabulatorLimit = 100
-const TabulatorMobileLimit = 100
+const TabulatorLimit = 100;
+const TabulatorMobileLimit = 100;
 
-const getTabulator = new Tabulator(TabulatorLimit)
+const getTabulator = new Tabulator(TabulatorLimit);
 getTabulator.startJob();
 
-const sendMobileTabulator = new TabulatorMobile(TabulatorMobileLimit)
+const sendMobileTabulator = new TabulatorMobile(TabulatorMobileLimit);
 sendMobileTabulator.startJob();
-
-
-
-
